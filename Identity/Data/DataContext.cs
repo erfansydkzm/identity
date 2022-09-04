52dba91b95489d1054c39a524e81c21f345d229d
erfansydkzm <seyedkazemierfan@yahoo.com>
2022-09-04 09:33:03 +0430

    CreateDb

---
+++ Identity/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,7 +1,12 @@
+using Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(option => option.UseSqlServer(
+    builder.Configuration.GetConnectionString("DafaultConnection")));
 
 var app = builder.Build();
 
